ef7673989c0416a9715c213ac1409caeb51f25bc
Antares <elgranpalicron@gmail.com>
2026-03-25 17:30:19 -0500

    Adbance in plressure plates

--- Source/TwistedShadows/Private/Activators/TS_PressurePlate.cpp
+++ Source/TwistedShadows/Private/Activators/TS_PressurePlate.cpp
@@ -3,19 +3,33 @@
 
 #include "Activators/TS_PressurePlate.h"
 
+#include "Components/BoxComponent.h"
+
 // Sets default values
 ATS_PressurePlate::ATS_PressurePlate()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(MeshComponent);
+
+	OverlapComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Overlap Component"));
+	OverlapComponent->SetupAttachment(MeshComponent);
+	OverlapComponent->SetCollisionResponseToAllChannels(ECR_Overlap);
+
+	bIsActivate = false;
+	bUseWeightToActivate = true;
+	MinWeightToActivate = 1.f;
 }
+
 
 // Called when the game starts or when spawned
 void ATS_PressurePlate::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	OverlapComponent->OnComponentBeginOverlap.AddDynamic(this, &ATS_PressurePlate::OnOverlapBegin);
+	OverlapComponent->OnComponentEndOverlap.AddDynamic(this, &ATS_PressurePlate::OnOverlapEnd);
 }
 
 // Called every frame
@@ -24,6 +38,19 @@ void ATS_PressurePlate::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+void ATS_PressurePlate::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Begin Overlap"));	
+}
+
+void ATS_PressurePlate::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if(GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("End Overlap"));	
+}
+
 
 void ATS_PressurePlate::ActivateActivatableActors_Implementation()
 {
--- Source/TwistedShadows/Public/Activators/TS_PressurePlate.h
+++ Source/TwistedShadows/Public/Activators/TS_PressurePlate.h
@@ -7,26 +7,48 @@
 #include "Interfaces/TS_Activator.h"
 #include "TS_PressurePlate.generated.h"
 
+class UBoxComponent;
+
 UCLASS()
 class TWISTEDSHADOWS_API ATS_PressurePlate : public AActor, public ITS_Activator
 {
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
+
 	ATS_PressurePlate();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
+	
 	virtual void Tick(float DeltaTime) override;
 	
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* MeshComponent;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	UBoxComponent* OverlapComponent;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	float MinWeightToActivate;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	uint8 bIsActivate : 1;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	uint8 bUseWeightToActivate : 1;
+	
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 	
 #pragma region ITS_Activatable
 
+public:
+	
 	virtual void ActivateActivatableActors_Implementation() override;
 	
 	virtual void DeactivateActivatableActors_Implementation() override;
@@ -34,6 +56,7 @@ public:
 	virtual bool IsActivatableActorEnabled_Implementation() const override;
 	
 	virtual TArray<TScriptInterface<ITS_Activatable>> GetActivatableActors_Implementation() const override;
+	
 #pragma endregion ITS_Activatable
 	
 };
