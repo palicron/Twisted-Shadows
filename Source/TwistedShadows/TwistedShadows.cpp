54d97e3a0334a9622a0e4bdfbf0f1d6195cc27b6
Antares <elgranpalicron@gmail.com>
2026-03-24 22:20:06 -0500

    Project intial push

---
+++ Source/TwistedShadows/TwistedShadows.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TwistedShadows.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TwistedShadows, "TwistedShadows" );
+
+DEFINE_LOG_CATEGORY(LogTwistedShadows)
+ 
