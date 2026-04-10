6bc6a40d9fa056357b7178caab569e1aaacdea35
TERAVISION\spalacios <jspalacios@teravisiongames.com>
2026-04-10 16:38:13 -0500

    Add test skeleton

---
+++ Source/TwistedShadows/Private/AutomationTest/ActivatorTestPresurePlate.cpp
@@ -0,0 +1,18 @@
+﻿#include "Misc/AutomationTest.h"
+#include "Tests/AutomationCommon.h"
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FActivatorTestPresurePlate,
+	"TwistedShadows.Activators.PresurePlate",
+	EAutomationTestFlags::EditorContext |
+	EAutomationTestFlags::ProductFilter
+)
+
+bool FActivatorTestPresurePlate::RunTest(const FString& Parameters)
+{
+    return true;
+ }
+#endif
